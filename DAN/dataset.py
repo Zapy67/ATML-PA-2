15206f3413fa98aaecf7cce745c6036b585dc546
Mohammad-Shayan-Asim <27100097@lums.edu.pk>
2025-10-08 23:37:47 +0500

    #3

--- DAN/dataset.py
+++ DAN/dataset.py
@@ -3,16 +3,16 @@ import deeplake
 sources = ["Art", "Realworld", "Product"]
 target = "Clipart"
 
-ds = deeplake.load('hub://activeloop/office-home-domain-adaptation')
+ds = deeplake.open('hub://activeloop/office-home-domain-adaptation')
 
-source_mask = [d in sources for d in ds["domain"].numpy()]
-target_mask = [d == target for d in ds["domain"].numpy()]
+# source_mask = [d in sources for d in ds["domain"].numpy()]
+# target_mask = [d == target for d in ds["domain"].numpy()]
 
-source_ds = ds[source_mask]
-target_ds = ds[target_mask]
+# source_ds = ds[source_mask]
+# target_ds = ds[target_mask]
 
-source_loader = source_ds.pytorch(num_workers = 0, batch_size= 4, shuffle = False, drop_last=True)
-target_loader = target_ds.pytorch(num_workers = 0, batch_size= 4, shuffle = False, drop_last=True)
+# source_loader = source_ds.pytorch(num_workers = 0, batch_size= 4, shuffle = False, drop_last=True)
+# target_loader = target_ds.pytorch(num_workers = 0, batch_size= 4, shuffle = False, drop_last=True)
 
 
 
